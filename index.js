1e149fe6db06e82374b87018b00e59ee564003ff
Kalok525 <kalvinhamilton16@gmail.com>
2023-03-22 19:52:13 +0200

    appened divs

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const container = document.getElementById('container')
+const container = document.getElementById('container');
+let grid = document.getElementsByClassName('grid')
+
+function createGrid(gridSize){
+    let size = gridSize * gridSize;
+    for (let i=0 ; i <= size ; i++){
+        grid = document.createElement('div')
+        grid.classList.add('grid')
+        container.appendChild(grid)
+    }
+}
+
+createGrid(16)
