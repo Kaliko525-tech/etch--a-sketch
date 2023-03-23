af9fa7d7aee081d9ab1b112d4aa6d17eb444240c
Kalok525 <kalvinhamilton16@gmail.com>
2023-03-23 17:22:52 +0200

    input new dynamic box function

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const container = document.getElementById('container');
-let grid = document.getElementsByClassName('grid')
 
-function createGrid(gridSize){
-    let size = gridSize * gridSize;
-    for (let i=0 ; i <= size ; i++){
-        grid = document.createElement('div')
-        grid.classList.add('grid')
-        container.appendChild(grid)
+function createBoxes(numberPerRow) {
+    const cdiv = document.querySelector('#container');
+    const total = (numberPerRow * numberPerRow) + numberPerRow;
+    const mod = numberPerRow + 1 ;
+
+    for (let i = 1; i < total; i++) {
+        const div = document.createElement('div');
+
+        if (i % mod === 0) {
+            div.style.cssText = "border: 0; height: 0; width: 100%";
+        } else {
+            div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
+        }
+
+        cdiv.appendChild(div);
     }
 }
 
-createGrid(16)
+createBoxes(16)
