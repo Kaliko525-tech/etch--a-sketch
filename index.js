0cfa1d8d02dbb0b9697fb844b8e650a433383bf4
Kalok525 <kalvinhamilton16@gmail.com>
2023-03-24 18:48:53 +0200

    added regEX numbers only for input

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const container = document.getElementById('container')
 let gDiv = document.getElementsByClassName('grid')
 const btn = document.getElementById("new-grid")
+const regex = /^[1-9]$|^[1-9][0-9]$|^(100)$/
 
 btn.addEventListener('click', () => changeGrid())
 
 function createGrid(number) {
     let total = (number * number) 
     for (i = 0; i < total ; i++ ) {
-        
         let cDiv = document.createElement('div') 
         cDiv.classList.add('grid') 
         
@@ -16,13 +16,19 @@ function createGrid(number) {
 }
 
 function changeGrid(){
-    input = prompt("Enter new grid size (1-100)");
+    input = prompt("Enter new grid size (1-100)") ;
+
+    if (input.match(regex)){
+        container.style.cssText = `grid-template-columns: repeat(${input},auto) ; grid-template-rows: repeat(${input},auto)`
+        container.replaceChildren();
     
-    container.style.cssText = `grid-template-columns: repeat(${input},auto) ; grid-template-rows: repeat(${input},auto)`
-    container.replaceChildren();
-    
-    createGrid(input)
+        createGrid(input)
+    } else {
+     alert("invalid input")
+    }
 }
 
 console.log(gDiv)
 createGrid(16)
+
+
