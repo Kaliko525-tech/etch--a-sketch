70781c57861b4aed9896076b07f6f1b7aea93152
Kalok525 <kalvinhamilton16@gmail.com>
2023-03-24 17:51:28 +0200

    created function to change grid size

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const container = document.getElementById('container')
 let gDiv = document.getElementsByClassName('grid')
+const btn = document.getElementById("new-grid")
+
+btn.addEventListener('click', () => changeGrid())
 
 function createGrid(number) {
     let total = (number * number) 
     for (i = 0; i < total ; i++ ) {
+        
         let cDiv = document.createElement('div') 
         cDiv.classList.add('grid') 
+        
         container.appendChild(cDiv)       
     }
 }
 
-createGrid(32)
-
+function changeGrid(){
+    input = prompt("Enter new grid size (1-100)");
+    
+    container.style.cssText = `grid-template-columns: repeat(${input},auto) ; grid-template-rows: repeat(${input},auto)`
+    container.replaceChildren();
+    
+    createGrid(input)
+}
 
 console.log(gDiv)
